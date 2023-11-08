fe67bff3cd58fc17cf604310a2bfa71d78cddded
mefa <mefaba@outlook.com>
2023-11-08 08:17:00 -0500

    Inirial setup with Javalin, HTMX, Thymeleaf, TailwindCSS

---
+++ src/main/java/org/webdemo/Main.java
@@ -0,0 +1,27 @@
+package org.webdemo;
+
+import io.javalin.Javalin;
+import io.javalin.http.Context;
+import io.javalin.http.Handler;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        Javalin app = Javalin.create();
+        app.get("/hello", ctx -> ctx.result("Hello World"));
+        app.get("/", new Handler() {
+            @Override
+            public void handle(@NotNull Context context) throws Exception {
+                Map<String,String> templateVariables = new HashMap<>();
+                templateVariables.put("message", "Allah Thymeleaf!");
+                context.render("templates/index.html",templateVariables);
+            }
+
+        });
+
+        app.start(7070);
+    }
+}
