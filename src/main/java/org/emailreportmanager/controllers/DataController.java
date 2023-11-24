9e012844c6d9ef189560012fb8106931f7cceb11
mefa <mefaba@outlook.com>
2023-11-23 19:42:12 -0500

    update-latest working version

--- src/main/java/org/emailreportmanager/controllers/DataController.java
+++ src/main/java/org/emailreportmanager/controllers/DataController.java
@@ -1,7 +1,6 @@
 package org.emailreportmanager.controllers;
 
 import io.javalin.http.Context;
-import io.javalin.http.Handler;
 import io.javalin.http.UploadedFile;
 import org.emailreportmanager.configurations.CsvConfiguration;
 import org.emailreportmanager.configurations.repository.CsvConfigurationRepository;
@@ -10,19 +9,33 @@ import org.springframework.data.jpa.repository.support.JpaRepositoryFactory;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class DataController {
-    public static void handlePostRequest(Context ctx) {
+    public static void handlePostRequest(Context ctx) throws IOException {
         // Access request body
-        String requestBody = ctx.body();
-        System.out.println("Hello");
-        System.out.println(requestBody);
+        UploadedFile uploaded_file = ctx.uploadedFiles().get(0);
+        InputStream inputStream = uploaded_file.getContent();
+        
+        OutputStream outputStream = new FileOutputStream("C:\\Temp\\text1.csv");
+        int data = uploaded_file.getContent().read();
+        while(data != -1) {
+            //do something with data...
+            outputStream.write(data);
+            data = inputStream.read();
 
+        }
+        inputStream.close();
+        outputStream.close();
+
+
+
+//        for (Map.Entry<String, List<String>> entry : requestBodyMap.entrySet()) {
+//            System.out.println(entry.getKey() + ": " + entry.getValue());
+//        }
         //populate entity
         CsvConfiguration csvConfiguration = new CsvConfiguration();
         csvConfiguration.setPath("/home/murat/IdeaProjects/ServerEmailScheduler/src/test/resources/test_0.csv");
