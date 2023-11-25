e4dcff6bac4d77f3a5752c17b1388b59f419130c
mefa <mefaba@outlook.com>
2023-11-25 09:18:11 -0500

    web update

--- src/main/java/org/emailreportmanager/Main.java
+++ src/main/java/org/emailreportmanager/Main.java
@@ -3,41 +3,50 @@ package org.emailreportmanager;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
 import io.javalin.http.Handler;
+import io.javalin.plugin.rendering.template.JavalinPebble;
+
 import org.emailreportmanager.controllers.DataController;
 import org.emailreportmanager.controllers.WebController;
 import org.jetbrains.annotations.NotNull;
+
+import com.mitchellbosecke.pebble.PebbleEngine;
+
 import org.emailreportmanager.services.H2ConsoleStarter;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
-    public static void main(String[] args) {
-        //START SQL SERVER
+    public static void main(String[] args) throws InterruptedException {
+        // START SQL SERVER
         H2ConsoleStarter h2ConsoleStarter = new H2ConsoleStarter();
         h2ConsoleStarter.startServer();
 
-        //SETUP APP SERVER
+        // Start template emgine
+        PebbleEngine engine = new PebbleEngine.Builder().cacheActive(false).build();
+        JavalinPebble.configure(engine);
+
+        // SETUP APP SERVER
         Javalin app = Javalin.create();
         app.get("/hello", ctx -> ctx.result("Hello World"));
         app.get("/", new Handler() {
             @Override
             public void handle(@NotNull Context context) throws Exception {
-                Map<String,String> templateVariables = new HashMap<>();
-                templateVariables.put("message", "Tanri Peeble!");
-                context.render("templates/index.peb",templateVariables);
+                Map<String, String> templateVariables = new HashMap<>();
+                templateVariables.put("message", "Tanri Pseeble!");
+                context.render("templates/index.peb", templateVariables);
             }
         });
 
-        //data handlers
-        app.post("/data", DataController::handlePostRequest);
+        // data handlers
+        app.post("/data", DataController::handleChartData);
+        app.post("/test-email", DataController::handleEmailData);
 
-
-
-        //website handlers
+        // website handlers
         app.get("/component-table", WebController::handlerComponentTable);
 
-        //START APP SERVER
+        // START APP SERVER
         app.start(7070);
+
     }
 }
--- src/main/java/org/emailreportmanager/controllers/DataController.java
+++ src/main/java/org/emailreportmanager/controllers/DataController.java
@@ -6,15 +6,9 @@ import org.emailreportmanager.components.Component;
 import org.emailreportmanager.components.TemplateHtml;
 import org.emailreportmanager.configurations.CsvConfiguration;
 import org.emailreportmanager.configurations.TableConfiguration;
-import org.emailreportmanager.configurations.repository.DataSourceConfigurationRepository;
 import org.emailreportmanager.services.EmailService;
 import org.emailreportmanager.services.GenerateComponent;
-import org.emailreportmanager.services.HtmlTableGenerator;
-import org.springframework.data.jpa.repository.support.JpaRepositoryFactory;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -23,65 +17,62 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DataController {
-    HtmlTableGenerator htmlTableGenerator= new HtmlTableGenerator();
-    public static void handlePostRequest(Context ctx) throws IOException {
-        // Recieve file from request body, Access request body
 
+    /*
+     * POST - /data
+     */
+    public static void handleChartData(Context ctx) throws IOException {
+        // Recieve file from request body, Access request body
         UploadedFile uploaded_file = ctx.uploadedFiles().get(0);
         InputStream inputStream = uploaded_file.getContent();
 
-        //Write to file
+        // Write to file
         OutputStream outputStream = new FileOutputStream("./tmp/test_email.csv");
         int data = uploaded_file.getContent().read();
-        while(data != -1) {
-            //do something with data...
+        while (data != -1) {
+            // do something with data...
             outputStream.write(data);
             data = inputStream.read();
         }
         inputStream.close();
         outputStream.close();
 
-        //populate entity
+        // Create a CsvConfiguration instance
+        // Set the path for the CSV file
+        // Set the separator for the CSV file (semicolon in this case)
         CsvConfiguration csvConfiguration = new CsvConfiguration();
         csvConfiguration.setPath("./tmp/test_email.csv");
         csvConfiguration.setSeparator(";");
-        //Result set produce
 
-        //Table generate
-
-        //Send response back to client
-
-
-
-
+        // Table generate
         TableConfiguration tableConfiguration = new TableConfiguration();
 
-        List<Component> componentList = new ArrayList<>();
         Component component = new GenerateComponent(csvConfiguration, tableConfiguration).getComponent();
-        TemplateHtml templateHtml = new TemplateHtml();
 
         String componentId = component.getComponentId();
         System.out.println(componentId);
-        templateHtml.setTemplateHtml("<html><body><h1>This is a Heading</h1><p>This is a paragraph.</p>"+ componentId + componentId+"<p>This is a footer.</p></body></html>");
 
-        componentList.add(component);
-
-        EmailService emailService = new EmailService(componentList, templateHtml);
-
-        emailService.getResult();
-
-        emailService.sendMail();
-
-
-
-
-
-
-        // Process the data as needed
-        // For example, you can parse JSON or perform other operations
-
-        // Send a response
-        ctx.status(200).result("POST request received successfully");
+        // Send a response to web-client
+        ctx.status(200).result(componentId);
     }
 
+    /*
+     * POST - /test-email
+     */
+    public static void handleEmailData(Context context) {
+        String dataEmail = context.formParam("data_email");
+
+        List<Component> componentList = new ArrayList<>();
+        /* STAART Email: Call this from email handler */
+        /*
+         * TemplateHtml templateHtml = new TemplateHtml();
+         * templateHtml.setTemplateHtml(dataEmail);
+         * 
+         * componentList.add(component);
+         * EmailService emailService = new EmailService(componentList, templateHtml);
+         * emailService.getResult();
+         */
+        /* END Email */
+        context.result("Noluyo");
+    }
 }
